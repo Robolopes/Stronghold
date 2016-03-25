7435cc2bc4d7c8af8f1dca86e03f1e20072565e9
Jamestristan <tristanfaass@yahoo.com>
2016-03-25 11:27:50 -0700

    Clean up imports.

--- Stronghold2016/src/org/usfirst/frc/team2339/robot/Robot.java
+++ Stronghold2016/src/org/usfirst/frc/team2339/robot/Robot.java
@@ -8,8 +8,6 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
 
 /**
  * The VM is configured to automatically run this class, and to call the
